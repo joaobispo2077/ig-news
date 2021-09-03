6b1375bea2cb6aa2031efc53eac30f44adca41b6
João Bispo <joaobispo2077@gmail.com>
2021-09-03 12:11:36 -0300

    feat: setup global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
@@ -7,7 +6,7 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1 className={styles.title}>Hello world</h1>
+      <h1 className="">Hello world</h1>
     </>
   );
 }
