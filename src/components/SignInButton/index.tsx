95333251c960184e6f9153eadd55e152de386359
João Bispo <joaobispo2077@gmail.com>
2021-09-04 12:35:14 -0300

    feat: add signIn with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import { signIn, signOut, useSession } from 'next-auth/client';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
 import styles from './styles.module.scss';
 
 export const SignInButton = () => {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button className={styles.signInButton} type="button">
       <FaGithub color="#04D361" /> joaobispo2077
       <FiX color="#737380" />
     </button>
   ) : (
-    <button className={styles.signInButton} type="button">
+    <button
+      className={styles.signInButton}
+      type="button"
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417" /> Sign with github
     </button>
   );
