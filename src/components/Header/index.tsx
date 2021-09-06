2a70c7ecff2792773a95462b0d6fef533cf688cf
João Bispo <joaobispo2077@gmail.com>
2021-09-06 11:59:30 -0300

    feat: redirect user to posts page when trying to make a subscription when already exists a subscription

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -19,7 +19,7 @@ export const Header = () => {
           <ActiveLink href="/" activeClassName={styles.active}>
             <a>Home</a>
           </ActiveLink>
-          <ActiveLink href="/posts" prefetch activeClassName={styles.active}>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
             <a>Posts</a>
           </ActiveLink>
         </nav>
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from 'next-auth/client';
+import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { getStripeClient } from '../../services/stripe/client';
 import styles from './styles.module.scss';
@@ -9,11 +10,16 @@ export type SubscribeButtonProps = {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
+  const router = useRouter();
 
   const handleSubscribe = async () => {
     if (!session) {
       signIn('github');
       return;
+    }
+
+    if (session.activeSubscription) {
+      return router.push('/posts');
     }
 
     try {
