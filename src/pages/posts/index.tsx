e88ae86a23371aa29dc0ea9a7c47c0f241248290
João Bispo <joaobispo2077@gmail.com>
2021-09-06 11:35:26 -0300

    feat: add next link into post list

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,11 @@
 import { GetStaticProps } from 'next';
 import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
-
 import Head from 'next/head';
+import Link from 'next/link';
+
 import { getPrismicClient } from '../../services/prismic';
+
 import styles from './styles.module.scss';
 
 type Post = {
@@ -25,11 +27,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a key={post.slug} href="#a">
-              <time>{post.updatedAt}</time>
-              <strong> {post.title}</strong>
-              <p>{post.excerpt}...</p>
-            </a>
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a href="#a">
+                <time>{post.updatedAt}</time>
+                <strong> {post.title}</strong>
+                <p>{post.excerpt}...</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
