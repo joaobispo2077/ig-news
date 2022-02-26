747f7b1443ebcc72459835b0934b66037683db13
João Bispo <joaobispo2077@gmail.com>
2022-02-26 10:20:25 -0300

    feat: setup tests

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  testIgnorePatterns: ['/node_modules/', '/dist/', '/.next/'],
+  setupFilesAFterEnv: ['<rootDir>/setupTests.ts'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+            decorators: true,
+          },
+          keepClassNames: true,
+          transform: {
+            legacyDecorator: true,
+            decoratorMetadata: true,
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
+        module: {
+          type: 'es6',
+          noInterop: false,
+        },
+      },
+    ],
+  },
+};
---
+++ setup.md
@@ -0,0 +1,5 @@
+Install jest to run tests:
+
+```bash
+yarn add jest jest-dom @testing-library/jest-dom @testing-library/react @testing-library/dom @testing-library/react-hooks @swc/core @swc/jest ts-node -D
+```
