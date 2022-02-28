aa8249e94ee522f60f1be78974dbd21a87a63b50
João Bispo <joaobispo2077@gmail.com>
2022-02-28 09:25:38 -0300

    test: add calls redirectToCheckout when user was logged in and has no subscription

--- tests/unit/components/SubscribeButton/SubscribeButton.spex.tsx
+++ tests/unit/components/SubscribeButton/SubscribeButton.spex.tsx
@@ -1,12 +1,16 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { Stripe } from '@stripe/stripe-js';
 import { render, fireEvent } from '@testing-library/react';
 import { signIn, useSession } from 'next-auth/client';
 import { NextRouter, useRouter } from 'next/router';
 import { SubscribeButton } from '../../../../src/components/SubscribeButton';
+import { api } from '../../../../src/services/api';
+import { getStripeClient } from '../../../../src/services/stripe/client';
 
 jest.mock('next-auth/client');
 jest.mock('next/router');
 jest.mock('../../../../src/services/stripe/client');
+jest.mock('../../../../src/services/api');
 
 describe('<SubscribeButton />', () => {
   it('should be able to calls signIn function when user is not signed in', () => {
@@ -48,21 +52,42 @@ describe('<SubscribeButton />', () => {
     expect(pushMock).toHaveBeenCalledWith('/posts');
   });
 
-  // it('should be able to calls redirectToCheckout logged in user and without subscription', () => {
-  //   const useSessionMocked = jest.mocked(useSession);
-  //   useSessionMocked.mockReturnValueOnce([
-  //     {
-  //       user: {
-  //         name: 'John Doe',
-  //         email: 'john.doe@example.com',
-  //         image: 'https://avatars2.githubusercontent.com/u/1234567?v=4',
-  //       },
-  //     },
-  //     true,
-  //   ]);
+  it('should be able to calls redirectToCheckout when user was logged in and has no subscription', () => {
+    const useSessionMocked = jest.mocked(useSession);
+    useSessionMocked.mockReturnValueOnce([
+      {
+        activeSubscription: false,
+      },
+      true,
+    ]);
 
-  //   const { getByText } = render(<SubscribeButton />);
+    const sessionId = 'odlkfhjdaskghsd-random-session-id';
 
-  //   expect(getByText('John Doe')).toBeInTheDocument();
-  // });
+    const apiMocked = api as jest.Mocked<typeof api>;
+    apiMocked.post.mockReturnValueOnce(
+      Promise.resolve({
+        data: {
+          sessionId,
+        },
+      }),
+    );
+
+    const getStripeClientMocked = jest.mocked(getStripeClient);
+    const redirectToCheckoutMock = jest.fn().mockResolvedValueOnce({});
+
+    getStripeClientMocked.mockResolvedValueOnce({
+      redirectToCheckout: redirectToCheckoutMock,
+    } as unknown as Stripe);
+
+    const { getByText } = render(<SubscribeButton priceId="abc" />);
+
+    const button = getByText('Subscribe now');
+    fireEvent.click(button);
+
+    expect(apiMocked.post).toHaveBeenCalledWith('/subscribe');
+    expect(getStripeClientMocked).toHaveBeenCalled();
+    expect(redirectToCheckoutMock).toHaveBeenCalledWith({
+      sessionId,
+    });
+  });
 });
