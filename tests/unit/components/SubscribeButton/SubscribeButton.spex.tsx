682ddf8b033458f43f7206e61237d31d4e7f3735
João Bispo <joaobispo2077@gmail.com>
2022-02-28 08:59:07 -0300

    test: add should calls sign in when user is not authenticated

---
+++ tests/unit/components/SubscribeButton/SubscribeButton.spex.tsx
@@ -0,0 +1,69 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { render, fireEvent } from '@testing-library/react';
+import { signIn, useSession } from 'next-auth/client';
+import { NextRouter, useRouter } from 'next/router';
+import { SubscribeButton } from '../../../../src/components/SubscribeButton';
+
+jest.mock('next-auth/client');
+jest.mock('next/router');
+jest.mock('../../../../src/services/stripe/client');
+
+describe('<SubscribeButton />', () => {
+  it('should be able to calls signIn function when user is not signed in', () => {
+    const useSessionMocked = jest.mocked(useSession);
+    useSessionMocked.mockReturnValueOnce([null, false]);
+
+    const signInMocked = jest.mocked(signIn);
+    signInMocked.mockReturnValueOnce(Promise.resolve());
+
+    const { getByText } = render(<SubscribeButton priceId="abc" />);
+
+    const button = getByText('Subscribe now');
+    fireEvent.click(button);
+
+    expect(signInMocked).toHaveBeenCalledWith('github');
+  });
+
+  it('should be able to redirect to /posts if user already have a subscription and is logged in', () => {
+    const useSessionMocked = jest.mocked(useSession);
+    useSessionMocked.mockReturnValueOnce([
+      {
+        activeSubscription: true,
+      },
+      true,
+    ]);
+
+    const useRouterMocked = jest.mocked<() => NextRouter>(useRouter);
+    const pushMocked = jest.mocked(
+      jest.fn((value: string) => Promise.resolve(value)),
+    );
+
+    useRouterMocked.mockReturnValueOnce({
+      route: {
+        push: pushMocked,
+      },
+    });
+
+    const { getByText } = render(<SubscribeButton priceId="abc" />);
+
+    expect(getByText('John Doe')).toBeInTheDocument();
+  });
+
+  // it('should be able to calls redirectToCheckout logged in user and without subscription', () => {
+  //   const useSessionMocked = jest.mocked(useSession);
+  //   useSessionMocked.mockReturnValueOnce([
+  //     {
+  //       user: {
+  //         name: 'John Doe',
+  //         email: 'john.doe@example.com',
+  //         image: 'https://avatars2.githubusercontent.com/u/1234567?v=4',
+  //       },
+  //     },
+  //     true,
+  //   ]);
+
+  //   const { getByText } = render(<SubscribeButton />);
+
+  //   expect(getByText('John Doe')).toBeInTheDocument();
+  // });
+});
